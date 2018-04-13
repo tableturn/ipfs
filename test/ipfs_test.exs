1a1b2637714a2bb0dcb0e0655cfab2c14a330742
MARTIN Pierre <hickscorp@gmail.com>
2018-04-13 20:08:29 +0100

    Adds a few tests covering IFPS.

--- test/ipfs_test.exs
+++ test/ipfs_test.exs
@@ -2,4 +2,26 @@ defmodule IPFSTest do
   @moduledoc false
   use Linky.DataCase, async: true
   doctest IPFS, import: true
+
+  setup :ipfs_conn
+
+  test "can fetch the node's version", %{conn: conn} do
+    {:ok, versions} = IPFS.version(conn)
+    assert is_map(versions)
+  end
+
+  test "can read dht keys", %{conn: conn} do
+    {:ok, keys} = IPFS.key_list(conn)
+    assert is_map(keys)
+  end
+
+  defp ipfs_conn(ctx) do
+    conn = %IPFS{
+      scheme: "http",
+      host: System.get_env("IPFS_HOST") || "localhost",
+      port: System.get_env("IPFS_PORT") || "9095"
+    }
+
+    {:ok, Map.put(ctx, :conn, conn)}
+  end
 end
