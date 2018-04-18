2b6fa74d4112d59fe6c8b20228ccc42122ebe4da
Pierre Martin <hickscorp@gmail.com>
2018-04-18 16:22:25 +0100

    Reworks repository after subtree fork to standalone.

---
+++ README.md
@@ -0,0 +1,20 @@
+# IPFS
+
+A library allowing an easy interaction with IPFS and IPFS Cluster nodes.
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `ipfs` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:ipfs, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/ipfs](https://hexdocs.pm/ipfs).
--- mix.exs
+++ mix.exs
@@ -5,23 +5,24 @@ defmodule IPFS.MixProject do
     [
       app: :ipfs,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      name: "IPFS",
+      source_url: "https://github.com/the-missing-link/ipfs",
+      homepage_url: "https://github.com/the-missing-link/ipfs",
+      docs: [extras: ~w(README.md)],
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: cli_env_for(:test, ~w(vcr.delete vcr.check vcr.show))
+      preferred_cli_env: cli_env_for(:test, ~w(
+        coveralls coveralls.detail coveralls.html coveralls.json coveralls.post
+        vcr.delete vcr.check vcr.show
+      ))
     ]
   end
 
   def application do
-    [
-      extra_applications: [:logger, :runtime_tools, :httpoison]
-    ]
+    [extra_applications: [:logger, :httpoison]]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -29,9 +30,17 @@ defmodule IPFS.MixProject do
 
   defp deps do
     [
+      # Dev only.
+      {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
+      {:credo, "~> 0.8", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.8", only: :test, runtime: false},
+      # Test only.
+      {:exvcr, "~> 0.10", only: :test},
+      # All environments.
       {:poison, "~> 3.1"},
-      {:httpoison, "~> 1.1"},
-      {:exvcr, "~> 0.10", only: :test}
+      {:httpoison, "~> 1.1"}
     ]
   end
 
