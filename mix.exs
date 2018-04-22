0913f321435a920b3335524618cdefd437eadfd3
Pierre Martin <hickscorp@gmail.com>
2018-04-22 17:44:35 +0100

    Bumps patch version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IPFS.MixProject do
   def project do
     [
       app: :ipfs,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
