892d12f65264e4e8b2a49cb2d6bb5a6d7560fb4e
Pierre Martin <hickscorp@gmail.com>
2018-11-28 21:45:10 +0000

    Updates dependencies, fixes VCR tapes, improves CI, bumps version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IPFS.MixProject do
   def project do
     [
       app: :ipfs,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
